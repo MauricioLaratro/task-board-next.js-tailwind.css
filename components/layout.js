15643cbf114979e11ba55e9c1e3feebd5b509be6
unknown <Mauricio Laratro>
2023-09-24 23:18:30 -0300

    Feature añadimos el <head> para cambiar el titulo, description y favicon

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,13 @@
+import Head from "next/head"
+
 function Layout({ children }) {
     return (
       <div className="flex flex-col h-screen bg-cyan-900">
+        <Head>
+          <title>Task Board</title>
+          <meta name="description" content="App de tareas para equipos de trabajo (Trello clone)." />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
         {children}
       </div>
     )
